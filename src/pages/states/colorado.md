fad58be70be9791ef96bc1ce5b0aeaf6bf0e4c3c
sneakyjoe007 <sneakyjoe007@users.noreply.github.com>
2022-05-17 23:00:04 -0400

    Added Colorado

---
+++ src/pages/states/colorado.md
@@ -0,0 +1,47 @@
+---
+heading: Great coffee with a conscience
+path: /colorado
+templateKey: product-page
+title: Our Coffee
+tags: ["colorado", "Denver Metro", "Colorado Springs"]
+image: /img/jumbotron.jpg
+description: Kaldi is the ultimate spot for coffee lovers who want to learn
+  about their java’s origin and support the farmers that grew it. We take coffee
+  production, roasting and brewing seriously and we’re glad to pass that
+  knowledge to anyone.
+intro:
+  blurbs:
+    - image: https://static.wixstatic.com/media/18a64d_0d8a8c156f9040269fe0590f6b9dfb2f~mv2.jpg/v1/fill/w_274,h_166,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/prime.jpg
+      text: |
+        Prime Volleyball
+    - image: https://static.wixstatic.com/media/18a64d_03692cd106e54d5ebe33ce707a0c5d3b~mv2.jpg/v1/fill/w_234,h_234,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/jugg.jpg
+      text: Juggernaut Volleyball
+    - image: https://static.wixstatic.com/media/18a64d_5e1cad4c37e146dc9bc30fb3599df147~mv2.jpg/v1/fill/w_354,h_154,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/rvc.jpg
+      text: Relentless Volleyball Club
+    - image: https://static.wixstatic.com/media/18a64d_eeb55c628e954841b4c6024359d65941~mv2.jpg/v1/fill/w_252,h_166,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/rebel.jpg
+      text: |
+        Rebel Volleyball Alliance
+  heading: What we offer
+  description: >
+    Kaldi is the ultimate spot for coffee lovers who want to learn about their
+    java’s origin and support the farmers that grew it. We take coffee
+    production, roasting and brewing seriously and we’re glad to pass that
+    knowledge to anyone. This is an edit via identity...
+main:
+  heading: Great coffee with no compromises
+  description: >
+    We hold our coffee to the highest standards from the shrub to the cup.
+    That’s why we’re meticulous and transparent about each step of the coffee’s
+    journey. We personally visit each farm to make sure the conditions are
+    optimal for the plants, farmers and the local environment.
+  image1:
+    alt: A close-up of a paper filter filled with ground coffee
+    image: /img/products-grid3.jpg
+  image2:
+    alt: A green cup of a coffee on a wooden table
+    image: /img/products-grid2.jpg
+  image3:
+    alt: Coffee beans
+    image: /img/products-grid1.jpg
+full_image: /img/products-full-width.jpg
+---
---
+++ src/templates/state-page.js
@@ -0,0 +1,186 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import FullWidthImage from "../components/FullWidthImage";
+
+// eslint-disable-next-line
+export const StatePageTemplate = ({
+  image,
+  title,
+  heading,
+  description,
+  intro,
+  main,
+  fullImage,
+}) => {
+  const heroImage = getImage(image) || image;
+  const fullWidthImage = getImage(fullImage) || fullImage;
+
+  return (
+    <div className="content">
+      <FullWidthImage img={heroImage} title={title} />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-7 is-offset-1">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  {heading}
+                </h3>
+                <p>{description}</p>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <Features gridItems={intro.blurbs} /> 
+                <div className="columns">
+                  <div className="column is-7">
+                    <h3 className="has-text-weight-semibold is-size-3">
+                      {main.heading}
+                    </h3>
+                    <p>{main.description}</p>
+                  </div>
+                </div>
+                <div className="tile is-ancestor">
+                  <div className="tile is-vertical">
+                    <div className="tile">
+                      <div className="tile is-parent is-vertical">
+                        <article className="tile is-child">
+                          <PreviewCompatibleImage imageInfo={main.image1} />
+                        </article>
+                      </div>
+                      <div className="tile is-parent">
+                        <article className="tile is-child">
+                          <PreviewCompatibleImage imageInfo={main.image2} />
+                        </article>
+                      </div>
+                    </div>
+                    <div className="tile is-parent">
+                      <article className="tile is-child">
+                        <PreviewCompatibleImage imageInfo={main.image3} />
+                      </article>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <FullWidthImage img={fullWidthImage} imgPosition={"bottom"} />
+    </div>
+  );
+};
+
+StatePageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+  main: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    image1: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    image2: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  }),
+  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+};
+
+const StatePage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <StatePageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+        main={frontmatter.main}
+        fullImage={frontmatter.full_image}
+      />
+    </Layout>
+  );
+};
+
+StatePage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
+
+export default StatePage;
+
+export const statePageQuery = graphql`
+  query StatePage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+          }
+        }
+        heading
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+        main {
+          heading
+          description
+          image1 {
+            alt
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 526, quality: 92, layout: CONSTRAINED)
+              }
+            }
+          }
+          image2 {
+            alt
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 526, quality: 92, layout: CONSTRAINED)
+              }
+            }
+          }
+          image3 {
+            alt
+            image {
+              childImageSharp {
+                gatsbyImageData(quality: 72, layout: FULL_WIDTH)
+              }
+            }
+          }
+        }
+
+        full_image {
+          childImageSharp {
+            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`;
